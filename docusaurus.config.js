a09ec24dc9ec16d31bdc92656d3875f2c2ff127b
bart_as93 <jablonski.bartosz93@gmail.com>
2024-05-08 12:23:22 +0200

    Add develop as copy branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,7 @@ const config = {
   organizationName: 'bartas93', // Usually your GitHub org/user name.
   projectName: 'bartas93.github.io', // Usually your repo name.
   deploymentBranch: 'main',
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   i18n: {
